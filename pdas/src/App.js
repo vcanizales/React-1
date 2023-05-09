3532b207472b07520321bc02b5097236e53dc33a
Valeria Canizales <vcanizales93@gmail.com>
2023-05-09 10:48:53 -0500

    importing CSS on react with Bulma library

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,13 +1,18 @@
-import ProfileCard from './ProfileCard'
+import 'bulma/css/bulma.css'
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images-jsx/alexa.png';
+import CortanaImage from './images-jsx/cortana.png';
+import SiriImage from './images-jsx/siri.png';
+
 
 function App() {
     return (
       <div>
           <div>Personal Digital Assistants</div>
 
-          <ProfileCard title= "Alexa" handle="@alexa99" />
-          <ProfileCard title= "Cortana" handle="@cortana32"/>
-          <ProfileCard title= "Siri" handle="@siri01"/>
+          <ProfileCard title= "Alexa" handle="@alexa99" image={AlexaImage} />
+          <ProfileCard title= "Cortana" handle="@cortana32" image={CortanaImage} />
+          <ProfileCard title= "Siri" handle="@siri01" image={SiriImage} />
       </div>
     );
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({title, handle}) {
+function ProfileCard({title, handle, image}) {
     // const title = props.title;
     // const handle = props.handle;
 
@@ -6,6 +6,7 @@ function ProfileCard({title, handle}) {
 
     return (
         <div>
+            <img src={image} />
             <div>Title is {title}</div>
             <div>Handle is {handle}</div>
         </div>
