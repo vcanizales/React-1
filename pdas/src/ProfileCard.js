730f3f061752f6e23f4fdfb2641f336ab0db162d
Valeria Canizales <vcanizales93@gmail.com>
2023-05-09 11:03:46 -0500

    working with css in order to organize cards in a container and columns

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -5,10 +5,19 @@ function ProfileCard({title, handle, image}) {
     // const { title, handle} = props;
 
     return (
-        <div>
-            <img src={image} />
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} />
+                </figure>
+            </div>
+
+                <div className="card-content">
+                    <div className="media-content">
+                    <p className="title-is-4">{title}</p>
+                    <p className="title-is-6">{handle}</p>
+                </div>
+            </div>
         </div>
     );
 }
