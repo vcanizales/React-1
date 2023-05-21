342db8568eb911127c051b32291348938ff1370a
Valeria Canizales <vcanizales93@gmail.com>
2023-05-21 07:56:42 -0500

    updated return statement, cleaned up index file, and entered term for the async response

--- pics/src/api.js
+++ pics/src/api.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID aEb9eObr-ZPJVFVWqJL8yLJ8OqneJnggHRymgodST8U'
         },
         params: {
-            query: 'cars'
+            query: term,
         }
     });
-    console.log(response);
-    return response;
+
+    return response.data.results;
 };
 
 export default searchImages;
